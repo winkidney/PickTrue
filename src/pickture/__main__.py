58491075877eb930418f12c1606c8f608664f67f
winkidney <winkidney@gmail.com>
2018-08-13 14:18:31 +0800

    Feature: Now the doenloader works well

--- src/pickture/__main__.py
+++ src/pickture/__main__.py
@@ -1,11 +1,35 @@
+import time
+
 import click
 
+from pickture.sites.artstation import ArtStation
 from .engine import Downloader
 
 
 @click.group('artstation')
-def art_station():
+def artstation():
     pass
 
-@art_station.command("user")
-def
+@click.argument("url")
+@artstation.command("user")
+def artstation_user(url):
+    site = ArtStation(url)
+    downloader = Downloader(save_dir=site.dir_name)
+    downloader.start()
+    downloader.add_task(
+        site.tasks
+    )
+    print("All task add...waiting for execution...")
+    try:
+        downloader.join()
+    except KeyboardInterrupt:
+        print("Exiting...Press crtl+c again to force quit")
+        downloader.stop()
+        exit(0)
+    else:
+        print("All task done...Enjoy!")
+
+
+if __name__ == "__main__":
+    artstation()
+
