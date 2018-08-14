1b9ecb273e01037fb19967c803097331a7dfd284
winkidney <winkidney@gmail.com>
2018-08-14 08:27:09 -0700

    Refactor: Move retry to utils

--- src/picktrue/utils.py
+++ src/picktrue/utils.py
@@ -1,3 +1,8 @@
+import time
+
+from functools import wraps
+from picktrue.logger import download_logger
+
 from threading import Thread
 
 
@@ -8,3 +13,24 @@ def run_as_thread(func, *args, name=None, **kwargs):
     t.setDaemon(True)
     t.start()
     return t
+
+
+def retry(max_retries=3):
+
+    def wrapper(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            retries = 0
+            while retries <= max_retries:
+                retries += 1
+                try:
+                    return func(*args, **kwargs)
+                except Exception:
+                    if retries > max_retries:
+                        download_logger.exception("Error occurs while execute function\n")
+                        break
+                    time.sleep(1)
+            return None
+        return wrapped
+
+    return wrapper
