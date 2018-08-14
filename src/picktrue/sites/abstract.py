3a07bcd93274b2c591cc81a1ad3bfb99a6984f92
winkidney <winkidney@gmail.com>
2018-08-14 08:18:25 -0700

    Refactor: Refactor fetcher with adapter pattern

--- src/picktrue/sites/abstract.py
+++ src/picktrue/sites/abstract.py
@@ -1,3 +1,4 @@
+import requests
 
 
 class DummySite:
@@ -5,3 +6,17 @@ class DummySite:
     @property
     def tasks(self):
         raise NotImplementedError()
+
+
+class DummyFetcher:
+
+    def __init__(self):
+        self.session = requests.session()
+
+    def get(self, url, **kwargs):
+        """
+        :rtype: requests.Response
+        """
+        if 'timeout' in kwargs:
+            kwargs.pop('timeout')
+        return self.session.get(url, timeout=(2, 30), **kwargs)
