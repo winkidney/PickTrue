4eb2c2d965b9241bb953191b15c9519a7edb4836
winkidney <winkidney@gmail.com>
2018-08-15 16:34:11 +0800

    Refactor: Add huaban tab and refactor all of the GUI structure

--- src/picktrue/gui/downloader.py
+++ src/picktrue/gui/downloader.py
@@ -2,88 +2,17 @@
 import os
 import time
 import tkinter as tk
-from tkinter import ttk, filedialog, messagebox as msgbox
+from tkinter import ttk
 
 from picktrue.gui.entry import art_station_run
+from picktrue.gui.toolkit import NamedInput, FileBrowse, StatusBar, info, ProgressBar
 from picktrue.utils import run_as_thread
 
 
-def info(message, title="信息"):
-    msgbox.showinfo(title=title, message=message)
-
-
-def get_working_dir():
-    return os.getcwd()
-
-
-class StatusBar(tk.Frame):
-    def __init__(self, master):
-        tk.Frame.__init__(self, master)
-        self.variable=tk.StringVar()
-        self.label=tk.Label(
-            self, bd=1, relief=tk.SUNKEN, anchor=tk.W,
-            textvariable=self.variable,
-            font=('arial', 16, 'normal')
-        )
-        self.variable.set('')
-        self.label.pack(fill=tk.X)
-        self.pack(fill=tk.BOTH)
-
-    def set(self, value):
-        self.variable.set(value)
-
-
-class NamedInput(tk.Frame):
-    def __init__(self, master=None, name=None, **kwargs):
-        super(NamedInput, self).__init__(master=master, **kwargs)
-        assert name is not None
-        label = tk.Label(self, text=name)
-        label.pack(side=tk.LEFT)
-
-        self.entry = tk.Entry(self)
-        self.entry.pack(side=tk.LEFT, fill=tk.X, expand=1)
-        self.pack(fill=tk.X)
-
-    def get_input(self):
-        return self.entry.get()
-
-
-class FileBrowse(tk.Frame):
-
-    def __init__(self, master=None, **kwargs):
-        super(FileBrowse, self).__init__(master=master, **kwargs)
-        self.label_text = tk.StringVar()
-        btn = tk.Button(self, text="下载到", command=self.choose_file)
-        btn.pack(
-            side=tk.LEFT,
-        )
-
-        tk.Label(self, textvariable=self.label_text).pack(
-            side=tk.LEFT,
-            fill=tk.X,
-        )
-        self.pack(fill=tk.X)
-
-        self.label_text.set(
-            get_working_dir()
-        )
-
-    def choose_file(self):
-        path = filedialog.askdirectory(
-            title="选择下载文件夹",
-        )
-        if not path:
-            return
-        self.label_text.set(path)
-
-    def get_path(self):
-        return self.label_text.get()
-
-
-class ArtStation(tk.Frame):
+class UserHomeDownloader(tk.Frame):
 
     def __init__(self, *args, **kwargs):
-        super(ArtStation, self).__init__(*args, **kwargs)
+        super(UserHomeDownloader, self).__init__(*args, **kwargs)
 
         self.downloader = None
         self.url = NamedInput(self, name="用户主页 ")
@@ -102,15 +31,12 @@ class ArtStation(tk.Frame):
         self.start_btn.pack(fill=tk.BOTH, expand=1)
         self.stop_btn.pack(fill=tk.BOTH, expand=1)
 
-        self.progress = ttk.Progressbar(
-            self,
-            orient="horizontal",
-            length=600,
-            mode="determinate",
-        )
-        self.progress.pack(expand=1)
+        self.progress = ProgressBar(self)
         self.status = StatusBar(self)
         self.start_update()
+
+    def run(self, url, path_prefix):
+        raise NotImplementedError()
 
     def start_download(self):
         url = self.url.get_input()
@@ -124,7 +50,7 @@ class ArtStation(tk.Frame):
         if self.downloader is not None:
             if not self.downloader.done:
                 return info("请停止后再重新点击下载...")
-        self.downloader = art_station_run(
+        self.downloader = self.run(
             url=url,
             path_prefix=path_prefix,
         )
@@ -147,48 +73,33 @@ class ArtStation(tk.Frame):
 
     def update_progress(self):
         if self.downloader is None:
-            self.progress['value'] = 0
-            self.progress['maximum'] = 100
+            self.progress.update_progress(
+                0, 100
+            )
             self.status.set("")
         else:
-            self.progress["value"] = self.downloader.counter.done
-            self.progress['maximum'] = self.downloader.counter.total
+            self.progress.update_progress(
+                self.downloader.counter.done,
+                self.downloader.counter.total,
+            )
             msg = self.downloader.counter.format()
             if self.downloader.done:
                 msg = msg + "  全部下载完毕，可以开始新的下载了：）"
             self.status.set(msg)
 
 
-class App(tk.Tk):
+class HuaBan(UserHomeDownloader):
 
-    def __init__(self, *args, **kwargs):
-        super(App, self).__init__(*args, **kwargs)
-        self.tabs = ttk.Notebook(self)
-        self.title("PickTrue - 相册下载器")
-        self.build_menu()
-        self._art_station = ArtStation(self)
-        self.tabs.add(self._art_station, text='ArtStation')
-        self.tabs.pack()
+    def run(self, url, path_prefix):
+        return art_station_run(
+            url=url,
+            path_prefix=path_prefix,
+        )
 
-    @staticmethod
-    def show_about():
-        info("作者：https://github.com/winkidney/")
+class ArtStation(UserHomeDownloader):
 
-    def build_menu(self):
-        menu = tk.Menu(self)
-        self.config(menu=menu)
-        main_menu = tk.Menu(menu)
-
-        # adds a command to the menu option, calling it exit, and the
-        # command it runs on event is client_exit
-        main_menu.add_command(label="关于", command=self.show_about)
-        menu.add_cascade(label="帮助", menu=main_menu)
-
-
-def run():
-    app = App()
-    app.mainloop()
-
-
-if __name__ == "__main__":
-    run()
+    def run(self, url, path_prefix):
+        return art_station_run(
+            url=url,
+            path_prefix=path_prefix,
+        )
---
+++ src/picktrue/gui/toolkit.py
@@ -0,0 +1,97 @@
+# coding: utf-8
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox as msgbox, ttk
+
+
+def info(message, title="信息"):
+    msgbox.showinfo(title=title, message=message)
+
+
+def get_working_dir():
+    return os.getcwd()
+
+
+class StatusBar(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.variable=tk.StringVar()
+        self.label=tk.Label(
+            self, bd=1, relief=tk.SUNKEN, anchor=tk.W,
+            textvariable=self.variable,
+            font=('arial', 16, 'normal')
+        )
+        self.variable.set('')
+        self.label.pack(fill=tk.X)
+        self.pack(fill=tk.BOTH)
+
+    def set(self, value):
+        self.variable.set(value)
+
+
+class NamedInput(tk.Frame):
+    def __init__(self, master=None, name=None, **kwargs):
+        super(NamedInput, self).__init__(master=master, **kwargs)
+        assert name is not None
+        label = tk.Label(self, text=name)
+        label.pack(side=tk.LEFT)
+
+        self.entry = tk.Entry(self)
+        self.entry.pack(side=tk.LEFT, fill=tk.X, expand=1)
+        self.pack(fill=tk.X)
+
+    def get_input(self):
+        return self.entry.get()
+
+
+class FileBrowse(tk.Frame):
+
+    def __init__(self, master=None, **kwargs):
+        super(FileBrowse, self).__init__(master=master, **kwargs)
+        self.label_text = tk.StringVar()
+        btn = tk.Button(self, text="下载到", command=self.choose_file)
+        btn.pack(
+            side=tk.LEFT,
+        )
+
+        tk.Label(self, textvariable=self.label_text).pack(
+            side=tk.LEFT,
+            fill=tk.X,
+        )
+        self.pack(fill=tk.X)
+
+        self.label_text.set(
+            get_working_dir()
+        )
+
+    def choose_file(self):
+        path = filedialog.askdirectory(
+            title="选择下载文件夹",
+        )
+        if not path:
+            return
+        self.label_text.set(path)
+
+    def get_path(self):
+        return self.label_text.get()
+
+
+class ProgressBar(ttk.Progressbar):
+
+    def __init__(self, master=None):
+        super(ProgressBar, self).__init__(
+            master=master,
+            orient="horizontal",
+            length=600,
+            mode="determinate",
+        )
+        self.pack(expand=1)
+
+    def update_progress(self, current, maximum=None):
+        self['value'] = current
+        if maximum is not None:
+            self['maximum'] = maximum
+
+
+    def reset_progress(self):
+        self.update_progress(0, 0)
