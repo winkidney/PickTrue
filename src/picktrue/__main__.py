dffb2e6ada16f5915849e8c87414ff2bd29a5528
winkidney <winkidney@gmail.com>
2018-08-15 15:32:15 +0800

    Feature: HuaBan now works well

--- src/picktrue/__main__.py
+++ src/picktrue/__main__.py
@@ -1,6 +1,7 @@
 import click
 
 from picktrue.sites.artstation import ArtStation
+from picktrue.sites.huaban import HuaBan
 from .engine import Downloader
 
 
@@ -16,6 +17,28 @@ def entry():
 )
 def artstation_user(url):
     site = ArtStation(url)
+    downloader = Downloader(fetcher=site.fetcher, save_dir=site.dir_name)
+    downloader.add_task(
+        site.tasks
+    )
+    print("All task add...waiting for execution...")
+    try:
+        downloader.join()
+    except KeyboardInterrupt:
+        print("Exiting...Press crtl+c again to force quit")
+        downloader.stop()
+        exit(0)
+    else:
+        print("All task done...Enjoy!")
+
+
+@click.argument("url")
+@entry.command(
+    "huaban-user",
+    help='download from huaban.com user home page',
+)
+def huban_user(url):
+    site = HuaBan(url)
     downloader = Downloader(fetcher=site.fetcher, save_dir=site.dir_name)
     downloader.add_task(
         site.tasks
