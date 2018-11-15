eb49d54314fbb41880db61c2aed2f327d120a6cf
winkidney <winkidney@gmail.com>
2018-11-15 21:28:47 +0800

    Bump version to 0.3.4

--- src/setup.py
+++ src/setup.py
@@ -13,7 +13,7 @@ install_requires = (
 
 setup(
     name='picktrue',
-    version='0.3.3',
+    version='0.3.4',
     packages=find_packages(HERE),
     install_requires=install_requires,
     url='https://github.com/winkidney/picktrue',
